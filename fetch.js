4a733a74baabc86819e4e32896f3c1a0b932847c
Stanko Arbutina <stanko.arbutina@gmail.com>
2019-10-09 11:08:00 +0200

    Extracted api to seperate module

--- fetch.js
+++ fetch.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const { range, pick } = require('lodash');
-const axios = require('axios');
+const { pick, get } = require('lodash');
 
 const argv = require('yargs')
     .usage('Usage: $0 <owner> <repo> [options]')
@@ -8,85 +7,40 @@ const argv = require('yargs')
     .describe('s', 'start page')
     .demandCommand(2)
     .argv;
-const [OWNER, REPO] = argv._;
-const START_PAGE = argv.start || 1;
 
-const stargazersURL = (owner, repo, page) => `https://api.github.com/repos/${owner}/${repo}/stargazers?page=${page}`;
-const ACCEPT_HEADER = {Accept: 'application/vnd.github.v3.star+json'};
-
-const CONFIG = {
+const [owner, repo] = argv._;
+const api = require('./lib/api')({
+    username: process.env.GITHUB_USERNAME,
     token: process.env.GITHUB_TOKEN,
-    username: process.env.GITHUB_USERNAME
-};
-const AUTH_OPTIONS = {auth: {username: CONFIG.username, password: CONFIG.token}};
+    startPage: argv.start || 1,
+    owner,
+    repo
+});
 
-async function request(url, options){
-    return axios.get(url, options);
-}
-
-function extractTotalPages(link){
-    let nextPart = link.split(',')[1];
-    if (nextPart) {
-        nextPart = nextPart.split('=')[1];
-        if (nextPart) {
-            nextPart = nextPart.split('>')[0];
-            if (nextPart) {
-                return Number(nextPart);
-            }
-        }
-    }
-    return null;
-}
-function joinLine(arr){
-    return arr.join("\t");
-}
+const joinLine = arr => arr.join("\t");
 const maybeNotAvailable = str => str || 'N/A';
-function formatResult(result){
-    return joinLine([result.login, result.email, result.location, result.name, result.starred_at].map(maybeNotAvailable));
-}
+const formatResult = result => joinLine([result.login, result.email, result.location, result.name, result.starred_at].map(maybeNotAvailable));
 
-async function processStargazers(data){
-    for (let userData of data) {
-        const response  =await request(userData.user.url, AUTH_OPTIONS);
-        const result = {...pick(response.data, ['email', 'name', 'location', 'login']), starred_at: userData.starred_at};
-        console.log(formatResult(result));
-    }
-}
-
-
-async function main(){
-    let parsedPages = false;
-    let to_process = [
-        stargazersURL(OWNER, REPO, START_PAGE)
-    ];
-
-    while (to_process.length) {
-        let nextUrl = to_process.shift();
-        const response = await request(nextUrl, {...AUTH_OPTIONS, headers: ACCEPT_HEADER});
-        if (!parsedPages) {
-            if (response.headers.link) {
-                const totalPages = extractTotalPages(response.headers.link);
-                if (totalPages && (totalPages > START_PAGE)) {
-                    const lower = START_PAGE + 1, upper = totalPages + 1;
-                    to_process = [
-                        ...to_process,
-                        ...range(lower, upper).map(
-                            pageNum => stargazersURL(OWNER, REPO, pageNum)
-                        )
-                    ];
-                }
-            }
-            console.log(joinLine(['username', 'email', 'location', 'name', 'starred_at']));
-            parsedPages = true;
+async function main() {
+    console.log(joinLine(['username', 'email', 'location', 'name', 'starred_at']));
+    while (!api.done()) {
+        const stargazersData = await api.nextPage();
+        for (let userData of stargazersData) {
+            const response = await api.geUserData(userData.user.url);
+            const result = {
+                ...pick(response.data, ['email', 'name', 'location', 'login']),
+                starred_at: userData.starred_at
+            };
+            console.log(formatResult(result));
         }
-        await processStargazers(response.data);
     }
 }
-
 
 main().then(
     () => process.exit(0),
     error => {
-        console.error(error.toString ? error.toString() : error);
+        const githubResponseError = get(error, 'response.data.message');
+        const errorText = githubResponseError || (error.toString ? error.toString() : error);
+        console.error(errorText);
     }
 );
